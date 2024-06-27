54449fd38247a4abd7a9e0760f3a64696f08d964
ihzorig <karaylaihzorig@gmail.com>
2024-06-27 14:51:01 +0800

    update

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -7,25 +7,30 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-
+  const [error, setError] = useState(""); // State for error message
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:8000/api/v1/auth", {
-        email,
-        password,
-      });
-      console.log(response.data);
-      if (response.data.success === true) {
-        navigate("/home"); // Navigate to '/home' on successful login
+      const response = await axios.post(
+        "http://localhost:2030/api/v1/auth/login",
+        {
+          email: email,
+          password: password,
+        }
+      );
+
+      console.log("Response from server:", response.data); // Log response for debugging
+
+      if (response.data.success) {
+        navigate("/home"); // Navigate on successful login
       }
+      setError(response.data.error); // Set error message
     } catch (error) {
-      console.error("Login failed:", error);
-      console.log("aldaa garlaa");
+      console.error("Error during login request:", error);
+      setError("Internetiin asuudal garlaa");
     }
   };
-
   return (
     <div className={css.background}>
       <div className={css.shape}></div>
@@ -33,7 +38,6 @@ const Login = () => {
 
       <form onSubmit={handleSubmit}>
         <h3>Login Here</h3>
-
         <label htmlFor="username">Email</label>
         <input
           type="text"
@@ -42,7 +46,6 @@ const Login = () => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-
         <label htmlFor="password">Password</label>
         <input
           type="password"
@@ -51,7 +54,7 @@ const Login = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-
+        {error && <p className={css.error}>{error}</p>}{" "}
         <button type="submit">Log In</button>
       </form>
     </div>
