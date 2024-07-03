ffe33c5848733a2d255926d03503dcd067c1e6a6
ihzorig <karaylaihzorig@gmail.com>
2024-07-03 15:35:44 +0800

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Route path="/" element={<Login />}></Route>
         <Route path="/home/*" element={<Home />}></Route>
         <Route path="/register" element={<Register />}></Route>
-        <Route path="*" element={<h1>Page not found</h1>}></Route>
+        <Route path="/*" element={<h1>Page not found</h1>}></Route>
       </Routes>
     </BrowserRouter>
   );
